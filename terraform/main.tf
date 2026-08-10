35900e81cf25b5950dbd27959f200d5ae854966c
Aditya Nutulapati <nsriaditya@IN-NSRIADI1-MBP.local>
2026-08-10 23:47:31 +0530

    fix: terraform fmt - remove trailing whitespace in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,7 +23,7 @@ data "aws_caller_identity" "current" {}
 
 locals {
   account_id = data.aws_caller_identity.current.account_id
-  
+
   common_tags = {
     Project     = var.project
     Environment = var.environment
